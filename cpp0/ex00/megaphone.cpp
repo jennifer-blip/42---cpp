de9d992844c369f3ee54320129196b24db07d07b
jennifer-blip <jodde@student.42.fr>
2026-01-26 15:18:40 +0100

    Add files via upload

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodde <jodde@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 13:34:19 by jodde             #+#    #+#             */
+/*   Updated: 2026/01/26 14:56:48 by jodde            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void	print_upper(const char *s)
+{
+	int	i;
+	char	u;
+
+	i = 0;
+	u = 0;
+	while(s[i])
+	{
+		u = std::toupper(s[i]);
+		std::cout << u;
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (i < (argc - 1))
+		print_upper(argv[i++]);
+	if (i == (argc - 1))
+	{
+		print_upper(argv[i]);
+		std::cout << std::endl;
+	}
+	return (1);
+}
+
