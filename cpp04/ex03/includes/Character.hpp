c728e289df96bf6e767fd04a6c4e50c01655a8f0
jennifer-blip <jodde@student.42.fr>
2026-03-13 15:47:42 +0100

    Add files via upload

---
+++ cpp04/ex03/includes/Character.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodde <jodde@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 14:53:21 by jodde             #+#    #+#             */
+/*   Updated: 2026/03/13 14:58:20 by jodde            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include "../includes/ICharacter.hpp"
+#include "../includes/AMateria.hpp"
+
+class	Character
+{
+};
